e35f9799e134960d8ceb9f749d6d175f6273e32d
David Kunzler <david.kunzler@mineral-minds.de>
2021-06-15 08:04:54 +0200

    log command for easier debugging, use provided filename also for pull

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,8 @@ eval `ssh-agent -s`
 
 ssh-add <(echo "$SSH_PRIVATE_KEY")
 
-remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; log 'Changing directory...' ; cd $WORKING_DIR ; log 'Launching docker-compose...' ; docker-compose pull ; docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans"
+remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; log 'Launching docker-compose...' ; docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" pull ; docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans"
+log "Command: $remote_command"
 
 echo ">> [local] Connecting to remote host."
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
